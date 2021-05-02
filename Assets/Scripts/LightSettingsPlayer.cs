5c2d91247a9760fa2946fb8d3f7787c49799b940
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2021-05-02 21:02:15 +0530

    Switching Between Nighyvision and Normal Camera

---
+++ Assets/Scripts/LightSettingsPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class LightSettingsPlayer : MonoBehaviour
+{
+    [SerializeField] PostProcessVolume myVolume;
+    [SerializeField] PostProcessProfile Standard;
+    [SerializeField] PostProcessProfile nightVision;
+    [SerializeField] GameObject nightVisisonOverlay;
+    
+    void Start()
+    {
+        myVolume.profile = Standard;
+        nightVisisonOverlay.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            if (myVolume.profile == Standard)
+            {
+                myVolume.profile = nightVision;
+                nightVisisonOverlay.SetActive(true);
+            }
+
+            else if(myVolume.profile==nightVision)
+            {
+                myVolume.profile = Standard;
+                nightVisisonOverlay.SetActive(false);
+            }
+        }
+    }
+}
