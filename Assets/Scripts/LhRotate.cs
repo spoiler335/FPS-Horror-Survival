c0ed5d2ad9b2e50a6dc9a244b193605eba6d4762
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2021-05-05 20:31:29 +0530

    Level Adjustments

---
+++ Assets/Scripts/LhRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LhRotate : MonoBehaviour
+{
+    [SerializeField] GameObject lhObject;
+    [SerializeField] float Speed = 0.1f;
+    
+    void Update()
+    {
+        lhObject.transform.Rotate(0f,Speed,0f,Space.World);
+    }
+}
