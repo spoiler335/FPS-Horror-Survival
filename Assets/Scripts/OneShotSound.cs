837517addae9051de932c680593d579e906f89c8
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2021-07-10 22:50:24 +0530

    One Shot Prefab

---
+++ Assets/Scripts/OneShotSound.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneShotSound : MonoBehaviour
+{
+    private AudioSource oneShot;
+    private Collider collider;
+
+    [SerializeField] bool oneTime=false;
+    [SerializeField] float pauseTime=5.0f;
+
+    void Start()
+    {
+        oneShot=GetComponent<AudioSource>();
+        collider=GetComponent<Collider>();
+    }
+
+     void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            oneShot.Play();
+            collider.enabled=false;
+
+            if(!oneTime)
+            {
+                StartCoroutine(Reset());
+            }
+            else
+            {
+                Destroy(gameObject,pauseTime);
+            }
+        }    
+    }
+
+    IEnumerator Reset() 
+    {  
+        yield return new WaitForSeconds(pauseTime);
+        collider.enabled=true;    
+    }
+
+    
+}
