fd2169ebfe2e81104346cb5de5113ba792e99663
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2021-07-14 23:15:48 +0530

    Inventory menu

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] GameObject inventoryMenu;
+    private bool inventoryActive=false;
+    void Start()
+    {
+        inventoryMenu.SetActive(false);
+        inventoryActive=false;
+        Cursor.visible=false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            if(inventoryActive)
+            {
+                inventoryMenu.SetActive(false);
+                inventoryActive=false;
+                Time.timeScale=1f;
+                Cursor.visible=false;
+            }
+            
+            else
+            {
+                inventoryMenu.SetActive(true);
+                inventoryActive=true;
+                Time.timeScale=0f;
+                Cursor.visible=true;
+            }
+        }
+    }
+}
