dc410126e9303fa9e920026b08c30272bc28637e
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2022-04-30 00:29:57 +0530

    Adding Sound to Doors

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -8,10 +8,22 @@ public class DoorScript : MonoBehaviour
     private Animator anim;
     public bool isOpen=false;
 
+    private AudioSource myPlayer;
+
+    [SerializeField] AudioClip cabinSound;
+    [SerializeField] AudioClip roomSound;
+    [SerializeField] AudioClip houseSound;
+
+    [SerializeField] bool isCabin;
+    [SerializeField] bool isRoom;
+    [SerializeField] bool isHouse;
+
+
     // Start is called before the first frame update
     void Start()
     {
         anim=GetComponent<Animator>();
+        myPlayer=GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,12 +38,47 @@ public class DoorScript : MonoBehaviour
         {
             anim.SetTrigger("Open");
             isOpen=true;
+            if(isCabin)
+            {
+                myPlayer.clip=cabinSound;
+                myPlayer.Play();
+            }
+            
+            if(isHouse)
+            {
+                myPlayer.clip=houseSound;
+                myPlayer.Play();
+            }
+            
+            if(isRoom)
+            {
+                myPlayer.clip=roomSound;
+                myPlayer.Play();
+            }
         }
 
         else if(isOpen)
         {
             anim.SetTrigger("Close");
             isOpen=false;
+
+            if(isCabin)
+            {
+                myPlayer.clip=cabinSound;
+                myPlayer.Play();
+            }
+            
+            if(isHouse)
+            {
+                myPlayer.clip=houseSound;
+                myPlayer.Play();
+            }
+            
+            if(isRoom)
+            {
+                myPlayer.clip=roomSound;
+                myPlayer.Play();
+            }
         }
     }
 }
