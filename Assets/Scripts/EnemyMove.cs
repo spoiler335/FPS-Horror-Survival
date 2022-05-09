3483a8e53acbaaf16ced935b6f99875f291c657f
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2022-05-09 22:49:48 +0530

    Enenmy Animation

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -7,19 +7,37 @@ public class EnemyMove : MonoBehaviour
 {
 
     private NavMeshAgent nav;
-
+    private Animator anim;
     private Transform target;
     private float distanceToTarget;
     private int targetNumber=1;
+    private bool hasStopped =false ;
 
     [SerializeField] float stopDistance=2f;
+    [SerializeField] int maxTargets=10;
+    [SerializeField] float waitTime=2.0f;
     [SerializeField] Transform tagret1;
     [SerializeField] Transform tagret2;
+
+    [SerializeField] Transform tagret3;
+    [SerializeField] Transform tagret4;
+
+    [SerializeField] Transform tagret5;
+    [SerializeField] Transform tagret6;
+    [SerializeField] Transform tagret7;
+    [SerializeField] Transform tagret8;
+    [SerializeField] Transform tagret9;
+    [SerializeField] Transform tagret10;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
         nav=GetComponent<NavMeshAgent>();
         target=tagret1;
+
+        anim=GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,20 +48,19 @@ public class EnemyMove : MonoBehaviour
         if(distanceToTarget>stopDistance)
         {
             nav.SetDestination(target.position);
+            nav.isStopped=false;
+            anim.SetInteger("State",0);
         }
 
         if(distanceToTarget<stopDistance)
         {
-            ++targetNumber;
-
-            if(targetNumber>2)
-            {
-                targetNumber=1;
-            }
-            setTarget();
+            nav.isStopped=true;
+            anim.SetInteger("State",1);
+            StartCoroutine(lookAround());
         }
     }
 
+    
     void setTarget()
     {
         if(targetNumber==1)
@@ -55,5 +72,66 @@ public class EnemyMove : MonoBehaviour
         {
             target=tagret2;
         }
+
+        if(targetNumber==3)
+        {
+            target=tagret3;
+        }
+
+        if(targetNumber==4)
+        {
+            target=tagret4;
+        }
+
+        if(targetNumber==5)
+        {
+            target=tagret5;
+        }
+
+        if(targetNumber==6)
+        {
+            target=tagret6;
+        }
+
+        if(targetNumber==7)
+        {
+            target=tagret7;
+        }
+
+        if(targetNumber==8)
+        {
+            target=tagret8;
+        }
+
+        if(targetNumber==9)
+        {
+            target=tagret9;
+        }
+
+        if(targetNumber==10)
+        {
+            target=tagret10;
+        }
     }
+
+    IEnumerator lookAround()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        if(!hasStopped)
+        {
+            hasStopped=true;
+            ++targetNumber;
+            if(targetNumber>maxTargets)
+            {
+                targetNumber=1;
+            }
+            setTarget();
+
+            yield return new WaitForSeconds(waitTime);
+            hasStopped=false;
+        }
+    }
+
+
 }
